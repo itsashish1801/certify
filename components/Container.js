585a82d1ef8da08143e91eb6b5be5bf35dff1bc3
Ashish Sharma <itsashishsharma1801@gmail.com>
2022-08-14 13:05:40 +0530

    Create a Container component.

---
+++ components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Container = () => {
+  return (
+    <div
+      className={clsx('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)}
+      {...props}
+    />
+  );
+};
+
+export default Container;
